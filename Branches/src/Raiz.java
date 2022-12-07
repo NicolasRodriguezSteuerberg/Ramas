2a4821855ef727adeaaa469ff2413eb91898ca4e
NicolasRodriguezSteuerberg <nrodriguezsteuerberg@danielcastelao.org>
2022-12-07 13:33:42 +0100

    Hotfix

--- Branches/src/Raiz.java
+++ Branches/src/Raiz.java
@@ -2,6 +2,6 @@ public class Raiz {
     public static void main(String[] args) {
         System.out.println("Hola");
 
-        System.out.println("Cambio rama");
+        //Arreglo codigo
     }
 }
